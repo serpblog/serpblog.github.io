97ad856191b5a04eb0270f146cbe865b1f462fd7
serverDream-Tailscale <serverDream@tom.com>
2024-02-05 11:30:26 +0800

    update

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/post/数据排序 第1课时.html","/post/C-七大经典排序算法详解.html","/post/数据排序 第2课时.html","/post/数据排序练习1.html"];function toRandomPost(){
+var posts=["/post/C-七大经典排序算法详解.html","/post/数据排序 第1课时.html","/post/数据排序 第2课时.html","/post/数据排序练习1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
